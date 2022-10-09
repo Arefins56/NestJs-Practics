51bf58d348f4eff1f7fa401f3f831411ed49366b
ferdows_semicolonit <ferdows.semicolonit@gmail.com>
2022-10-09 11:17:07 +0600

    pp

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,6 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -16,24 +9,36 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get('/')
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getAllTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    console.log(filterDto);
+
+    if(Object.keys(filterDto).length){
+      return this.tasksService.getTaskWithFilters(filterDto);
+    }else{
+      return this.tasksService.getAllTasks();
+    }
+    
+    // return this.tasksService.getAllTasks();
   }
+
 
   @Get('/:id')
   getTaskById(@Param('id') id: string): Task {
     return this.tasksService.getTaskById(id);
   }
 
+
   @Post('/')
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
   }
 
+
   @Delete('/:id')
   deleteTask(@Param('id') id: string): void {
     this.tasksService.deleteTask(id);
   }
+
 
   @Patch('/:id/status')
   updateTaskStatus(
@@ -42,4 +47,5 @@ export class TasksController {
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
+  
 }
